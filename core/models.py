90dbf896bd1f909a5568a05eb1edb48de52fa8e3
Michele Mattioni <mattions@gmail.com>
2013-04-18 21:21:37 +0100

    First take to custom user, not working yet

--- core/models.py
+++ core/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from django.contrib.auth.models import AbstractBaseUser
 
 # Create your models here.
+
+class CustomUser(AbstractBaseUser):
+
+    date_of_birth = models.DateField()
+    height = models.FloatField()
+    REQUIRED_FIELDS = ['date_of_birth', 'height']
+    USERNAME_FIELD = 'username'
